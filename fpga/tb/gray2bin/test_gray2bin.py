bee61bfeac838b3b70599f46860d4aef8e0c9a58
Chris H. Meyer <christian.h.meyer@t-online.de>
2023-08-20 18:48:55 +0200

    Add gray2bin TB

---
+++ fpga/tb/gray2bin/test_gray2bin.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+"""
+Copyright (c) 2023 Chris H. Meyer
+
+This file is part of aRTS.
+
+aRTS is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+aRTS is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+for more details.
+
+You should have received a copy of the GNU General Public License along
+with aRTS. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import itertools
+import logging
+import os
+
+import cocotb_test.simulator
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge, FallingEdge, ClockCycles
+from cocotb.regression import TestFactory
+
+import random
+
+class TB:
+    def __init__(self, dut):
+        self.dut = dut
+
+        self.log = logging.getLogger("cocotb.tb")
+        self.log.setLevel(logging.DEBUG)
+
+        cocotb.start_soon(Clock(dut.clk, 8, units="ns").start())
+
+    async def reset(self):
+        self.dut.rst.setimmediatevalue(0)
+        await RisingEdge(self.dut.clk)
+        await RisingEdge(self.dut.clk)
+        self.dut.rst.value = 1
+        await RisingEdge(self.dut.clk)
+        await RisingEdge(self.dut.clk)
+        self.dut.rst.value = 0
+        await RisingEdge(self.dut.clk)
+        await RisingEdge(self.dut.clk)
+
+
+# From https://rosettacode.org/wiki/Gray_code#Python:_on_integers
+def gray_encode(n):
+    return n ^ n >> 1
+
+
+# From https://rosettacode.org/wiki/Gray_code#Python:_on_integers
+def gray_decode(n):
+    m = n >> 1
+    while m:
+        n ^= m
+        m >>= 1
+    return n
+
+
+async def run_test(dut):
+    random.seed(0)
+
+    tb = TB(dut)
+
+    clk = Clock(dut.clk, 8, units="ns")  # Create a 8ns period clock on port clk
+    cocotb.start_soon(clk.start())  # Start the clock
+
+    await tb.reset()
+    await FallingEdge(dut.clk)
+
+    for _ in range(123456):
+        random_num = random.randrange(0, 2**32)
+
+        dut.gray.value = gray_encode(random_num)
+        await FallingEdge(dut.clk)
+
+        assert random_num == dut.bin.value
+
+    await ClockCycles(dut.clk, 10)
+
+
+if cocotb.SIM_NAME:
+    factory = TestFactory(run_test)
+    factory.generate_tests()
